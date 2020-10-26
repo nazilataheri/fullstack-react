afca4642c0d2217f3d085adbefc790eb2e8a08bc
nazilataheri@gmail.com <nazila@SanickBook-Air.local>
2020-10-26 18:51:44 +1100

    update to high order component

--- README.md
+++ README.md
@@ -2,5 +2,7 @@ Fullstack React app
 to run:
 
 1)yarn
+
 2)yarn webpack
+
 3)yarn dev
--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,11 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
       },
       {
         test: /\.json5$/i,
