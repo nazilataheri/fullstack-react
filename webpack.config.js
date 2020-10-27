1fab27801feba263388cf64d0a8895054b22e761
nazilataheri@gmail.com <nazila@SanickBook-Air.local>
2020-10-27 14:41:33 +1100

    update babel plugins in config files

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,18 @@ module.exports = {
         use: [
           {
             loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-react",
+                "@babel/preset-env",
+                {
+                  plugins: [
+                    "@babel/plugin-proposal-class-properties",
+                    "@babel/plugin-proposal-object-rest-spread",
+                  ],
+                },
+              ],
+            },
           },
         ],
       },
